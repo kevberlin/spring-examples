ab13eed29c8aa2aac924aa3f9d4172397e45e7b4
Anonymous <>
2018-11-08 15:08:10 +0100

    splitter

---
+++ 08 Donnerstag/basic-file-integration/src/main/java/com/example/basicfileintegration/RouterConfiguration.java
@@ -0,0 +1,82 @@
+package com.example.basicfileintegration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.Router;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.annotation.Splitter;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.router.AbstractMessageRouter;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHandler;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Configuration
+public class RouterConfiguration {
+
+    @Bean
+    @Router(inputChannel = "numberChannel")
+    public AbstractMessageRouter evenOddRouter() {
+        return new AbstractMessageRouter() {
+            @Override
+            protected Collection<MessageChannel>
+            determineTargetChannels(Message<?> message) {
+                Integer number = (Integer) message.getPayload();
+                if (number % 2 == 0) {
+                    return Collections.singleton(evenChannel());
+                }
+                return Collections.singleton(oddChannel());
+            }
+        };
+    }
+
+    @Bean
+    public MessageChannel evenChannel() {
+        return new DirectChannel();
+    }
+
+    @Bean
+    public MessageChannel oddChannel() {
+        return new DirectChannel();
+    }
+
+    @Bean
+    public MessageChannel numberChannel() {
+        return new DirectChannel();
+    }
+
+    @Bean
+    @ServiceActivator(inputChannel = "evenChannel")
+    public MessageHandler sysoutEvenHandler() {
+        return message -> {
+            System.out.println("Even: " + message.getPayload());
+        };
+    }
+
+    @Bean
+    @ServiceActivator(inputChannel = "oddChannel")
+    public MessageHandler sysoutOddHandler() {
+        return message -> {
+            System.out.println("Odd:  " + message.getPayload());
+        };
+    }
+
+    @Bean
+    public MessageChannel csvChannel() {
+        return new DirectChannel();
+    }
+
+    @Splitter(inputChannel="csvChannel", outputChannel="numberChannel")
+    public List<Integer> lineItemSplitter(String text) {
+        String[] strings = text.split(",");
+        return Stream.of(strings)
+                .map(s -> Integer.valueOf(s))
+                .collect(Collectors.toList());
+    }
+}
--- 08 Donnerstag/basic-file-integration/src/test/java/com/example/basicfileintegration/CreateFileTest.java
+++ 08 Donnerstag/basic-file-integration/src/test/java/com/example/basicfileintegration/CreateFileTest.java
@@ -5,9 +5,13 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.integration.router.AbstractMessageRouter;
 import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHandler;
 import org.springframework.messaging.support.GenericMessage;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.sound.midi.Receiver;
 
 import static junit.framework.TestCase.assertTrue;
 
@@ -19,9 +23,29 @@ public class CreateFileTest {
     @Autowired
     private MessageChannel textChannel;
 
+    @Qualifier("numberChannel")
+    @Autowired
+    private MessageChannel numberChannel;
+
+    @Qualifier("csvChannel")
+    @Autowired
+    private MessageChannel csvChannel;
+
     @Test
     public void sendTextAndExpectFileCreation() {
         boolean send = textChannel.send(new GenericMessage<>("String"));
         assertTrue(send);
+    }
+
+    @Test
+    public void sendMultipleNumbers() {
+        for(int i=0;i<5;i++) {
+            numberChannel.send(new GenericMessage<>(i));
+        }
+    }
+    
+    @Test
+    public void testCsvSplitter() {
+        csvChannel.send(new GenericMessage<>("1,2,3,4,5,6,7,8,9"));
     }
 }
