c386c4042e081449fd9699cf31a6ea42f9b5356d
Anonymous <>
2018-11-08 10:49:32 +0100

    websockets

---
+++ 08 Donnerstag/sockets/src/main/java/com/example/sockets/GreetingController.java
@@ -0,0 +1,18 @@
+package com.example.sockets;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
+public class GreetingController {
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        String htmlEscape = HtmlUtils.htmlEscape(message.getName());
+        return new Greeting("Hello, " + htmlEscape + "!");
+    }
+}
---
+++ 08 Donnerstag/sockets/src/main/java/com/example/sockets/SocketsApplication.java
@@ -0,0 +1,12 @@
+package com.example.sockets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocketsApplication.class, args);
+	}
+}
